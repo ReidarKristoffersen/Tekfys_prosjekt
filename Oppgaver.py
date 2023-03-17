540eb5d338d393674e04ad70ddc8d359d79b0bad
Thientn1 <thiennguyen1033@gmail.com>
2023-03-17 12:13:05 +0100

    Update Oppgaver.py

--- Oppgaver.py
+++ Oppgaver.py
@@ -1,7 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 17 11:34:50 2023
+import numpy as np
+import matplotlib.pyplot as plt
 
-@author: krist
-"""
+# PARAMETERE
+
+R = 8.314             #[J / mol K]
+T_c = 647.096         #[K]
+p_c = 22.064 * 10e6   #[Pa] 
+#n = 1                 #[mol] 
+
+
+# OPPGAVE 1a)
+
+a = (27 * R**2 * T_c**2) / (64 * p_c) []
+b = (R * T_c) / (8 * p_c)
+V_c = 3*b
+
+print(a, b, V_c)
+#Sammenlign V_c med eksperimentelle verdier
+#Eksperimentell V_c = 55.948mL
+
+
+# OPPGAVE 1b)
 
