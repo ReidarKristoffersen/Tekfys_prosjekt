e8c1706048709bce41e63b6088841bfab25b5b01
ReidarKristoffersen <kristoffersenreidar@gmail.com>
2023-03-17 13:00:29 +0100

    Update Oppgaver.py

--- Oppgaver.py
+++ Oppgaver.py
@@ -11,7 +11,7 @@ p_c = 22.064 * 10e6   #[Pa]
 
 # OPPGAVE 1a)
 
-a = (27 * R**2 * T_c**2) / (64 * p_c) []
+a = (27 * R**2 * T_c**2) / (64 * p_c)
 b = (R * T_c) / (8 * p_c)
 V_c = 3*b
 
@@ -22,3 +22,103 @@ print(a, b, V_c)
 
 # OPPGAVE 1b)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def ex_eq(c):
+    return (np.sinh(2*c/T_c))**2-1
+
+def d_ex_eq(c):
+    return 2*np.sinh(4*c/T_c)/T_c
+
+
+def newton_one_var(f, df, c_0, tol):
+    max_it =100000
+    i = 0
+    x = np.array([c_0])
+    while(np.abs(f(c_0))>=tol):
+        x_i = x[-1] - (f(x[-1])/df(x[-1]))
+        x = np.append(x, x_i)
+        i+=1
+        if(i>max_it):
+            raise Exception("Maximum iterations reached")
+    print(f"The root was found to be at {x[-1]} after {i} iterations")
+    return x
+
+c_0 = 1
+c_arr = newton_one_var(ex_eq, d_ex_eq, c_0, 10e-2)
