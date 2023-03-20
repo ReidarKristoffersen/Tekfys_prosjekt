e91f3b34e0634328e40be2b1d38bedecc547c8c2
Thientn1 <thiennguyen1033@gmail.com>
2023-03-20 11:07:50 +0100

    Update Oppgaver.py

--- Oppgaver.py
+++ Oppgaver.py
@@ -11,14 +11,31 @@ p_c = 22.064 * 10e6   #[Pa]
 
 # OPPGAVE 1a)
 
-a = (27 * R**2 * T_c**2) / (64 * p_c) []
-b = (R * T_c) / (8 * p_c)
-V_c = 3*b
+#Fra nettet (engineeringtoolbox)
+a = 5.537                              #[bar * L^2 / mol^2]
+a = a * 1000**2                        #[bar * mL^2 / mol^2]
+b = 0.03049                            #[L / mol]
+b = b * 1000                           #[mL / mol]
+V_c = 3 * b
+
 
 print(a, b, V_c)
-#Sammenlign V_c med eksperimentelle verdier
-#Eksperimentell V_c = 55.948mL
+#Eksperimentell V_c = 55.948mL.
+#Her ser vi at volumet er 91.47 mL. Man kan bare få samsvar med maks to termodynamiske variable. Her: p_c og T_c.
 
 
 # OPPGAVE 1b)
 
+V_val = np.linspace(75, 300, 1000)     #[mL]
+vdW_tilst_likn = ((R * T_c) * 10 / (V_val - b)) - (a / V_val**2)
+
+plt.figure(figsize=(15,8))
+plt.plot(V_val, vdW_tilst_likn)
+plt.xlabel("V [ml]")
+plt.ylabel("p [bar]")
+plt.title("1 mol $H_2 O$, van der Waals isoterm, T = 647.096 K")
+plt.grid()
+plt.show() 
+
+#Det som kan observeres her som er ulikt fra plottet i oppgaveteksten er at her så er den avtagende hele veien. I oppgaveteksten øker den først til kritisk punkt før den blir avtagende.
+#Det vi også kan se er at trykket er høyere for alle verdier av temperaturen.
