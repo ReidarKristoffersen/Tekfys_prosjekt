c60c22c2cd0deafd83a90f9d46603ebf8e6783c8
ReidarKristoffersen <kristoffersenreidar@gmail.com>
2023-03-24 17:53:41 +0100

    la til data for fltende vann. fiksa 1e) med enheter:)

--- Oppgaver.py
+++ Oppgaver.py
@@ -1,13 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 import sympy 
 
 # PARAMETERE
 
 R = 8.314             #[J / mol K]
+R = 8314.46261815324  #[L Pa/ mol K]
+R = R*1000            #[mL Pa/ mol K]
+R = R*10**(-5)        #[mL bar/ mol K]
 T_c = 647.096         #[K]
 p_c = 22.064 * 10e6   #[Pa] 
-#n = 1                 #[mol] 
+p_c = p_c*10**(-5)    #[bar] 
+#n = 1                #[mol] 
 
 
 # OPPGAVE 1a)
@@ -28,7 +33,27 @@ print(a, b, V_c)
 # OPPGAVE 1b)
 
 V_val = np.linspace(75, 300, 1000)     #[mL]
-vdW_tilst_likn = ((R * T_c) * 10 / (V_val - b)) - (a / V_val**2)
+
+def vdW(T,V):
+    """
+    Uses Van der Waals tilstandsligning for å 
+
+    Parameters
+    ----------
+    T : float
+        Temperatur
+    V : float
+        Volum
+
+    Returns
+    -------
+    p : float
+        Trykk ved gitt temperatur og volum
+
+    """
+    p= ((R * T) / (V - b)) - (a / V**2)
+    return p
+vdW_tilst_likn = vdW(T_c, V_val)
 
 plt.figure(figsize=(15,8))
 plt.plot(V_val, vdW_tilst_likn)
@@ -174,8 +199,8 @@ def newton_one_var(f, df, T_0, tol=10e-12):
     x = np.array([T_0])
     while(np.abs(f(x[-1]))>=tol):
         x_i = x[-1] - (f(x[-1])/df(x[-1]))
-        print(f"f: {f(x[-1])}")
-        print(f"df: {df(x[-1])}")
+        #print(f"f: {f(x[-1])}")
+        #print(f"df: {df(x[-1])}")
         x = np.append(x, x_i)
         i+=1
         if(i>max_it):
@@ -200,7 +225,7 @@ print(r-2*c/np.log(1+np.sqrt(2))) #Sjekker at løsningen stemmer med ligningen g
 e_i = np.abs(T_arr-r) #definer array med feil for hver iterasjon
 plt.plot(e_i)
 plt.show()
-p_i = np.log(e_i[2:]/e_i[1:-1])/np.log(e_i[1:-1]/e_i[0:-2]) #Tar ikke med første element ettersom vi her fikk en rar verdi i plottet?????
+p_i = np.log(e_i[2:]/e_i[1:-1])/np.log(e_i[1:-1]/e_i[0:-2]) 
 plt.plot(p_i)
 plt.title("$p_i$, konvergerer mot q")
 plt.xlabel("i")
@@ -208,6 +233,11 @@ plt.ylabel("$p_i$")
 plt.show()
 
 #Ser at p_i konvergerer mot 2. Dette er q-verdien samsvarer med at Newtons metode konvergerer kvadratisk
+
+
+#1e)
+"""
+SYMPY GREIER, IKKE I BRUK PER NÅ
 
 def func1():
     return R_sym*T_sym*(1 / (V_g_sym - b_sym) - 1 / (V_v_sym - b_sym)) - a_sym*(1 / V_g_sym**2 - 1 / V_v_sym**2)  # 11
@@ -228,10 +258,9 @@ df2_dv = sympy.diff(f2, V_v_sym)
 
 Jacobi_uneval = sympy.Matrix([[df1_dg, df1_dv], [df2_dg, df2_dv]])
 
-print(Jacobi_uneval)
 def Jacobi(J, x0, y0,T):
     
-    """
+   
     Parameters
     ----------
     J : Array
@@ -244,27 +273,47 @@ def Jacobi(J, x0, y0,T):
     -------
     Jacobi : Array
              Evaluated Jacobi determinant at (x0, y0)
-    """
+    
     return np.array(J.subs([(a_sym, a), (b_sym, b), (R_sym, R), (T_sym, T), (V_g_sym, x0), (V_v_sym, y0)]), dtype=np.float64)
-
+"""
 def f1(V_g, V_v, T):
     return R*T*(1 / (V_g - b) - 1 / (V_v - b)) - a*(1 / V_g**2 - 1 / V_v**2)  # 11
 
 def f2(V_g, V_v, T):
     return R*T*(np.log((V_g - b) / (V_v - b)) / (V_g - V_v) ) - a*(1 / (V_g*V_v) - 1 / (V_g**2)) - R*T/(V_g-b) # 12
 
-def f_tot(V_g, V_v, T):
-    return np.array([f1(V_g, V_v, T), f2(V_g, V_v, T)]).T
+def f_tot(V_arr, T=273):
+    return np.array([f1(V_arr[0], V_arr[1], T), f2(V_arr[0], V_arr[1], T)]).T
 
-#J = sympy.Matrix([func1, func2]).jacobian([V_g, V_v])
-#print(J)
 
 def Jacobi_numpy(V_g,V_v,T):
-    J = np.array([[-R*T/(V_g - b)**2 + 2*a/V_g**3, R*T/(V_v - b)**2 - 2*a/V_v**3], [R*T/(V_g - b)**2 + R*T/((V_g - V_v)*(V_g - b)) - R*T*np.log((V_g - b)/(V_v - b))/(V_g - V_v)**2 - a*(-1/(V_g**2*V_v) + 2/V_g**3), -R*T/((V_g - V_v)*(V_v - b)) + R*T*np.log((V_g - b)/(V_v - b))/(V_g - V_v)**2 + a/(V_g*V_v**2)]])
+    """
+    Returrnerer jacobimatrisa ved gitt V_g og V_v
+
+    Parameters
+    ----------
+    V_g : float
+        Gassvolum
+    V_v : float
+        væskevolum
+    T : float
+        Temperature.
+
+    Returns
+    -------
+    J : Array 2x2
+        Jacobi
+
+    """
+    df1_dg = -R*T/(V_g - b)**2 + 2*a/V_g**3
+    df1_dv=R*T/(V_v - b)**2 - 2*a/V_v**3
+    df2_dg = R*T/(V_g - b)**2 + R*T/((V_g - V_v)*(V_g - b)) - R*T*np.log((V_g - b)/(V_v - b))/(V_g - V_v)**2 - a*(-1/(V_g**2*V_v) + 2/V_g**3)
+    df2_dv = -R*T/((V_g - V_v)*(V_v - b)) + R*T*np.log((V_g - b)/(V_v - b))/(V_g - V_v)**2 + a/(V_g*V_v**2)
+    J = np.array([[df1_dg, df1_dv], [df2_dg, df2_dv]])
     return J
 
 
-def newton_two_var(f, Jacobi_uneval, V_arr, T, tol = 10e-1):
+def newton_two_var(f, V_arr, T, tol = 10e-12):
     """
     
 
@@ -287,15 +336,15 @@ def newton_two_var(f, Jacobi_uneval, V_arr, T, tol = 10e-1):
     """
     max_it = 100
     i = 0
-    while np.linalg.norm(f(V_arr[0], V_arr[1], T)) >= tol:
+    while np.linalg.norm(f(V_arr, T)) >= tol:
         #print(V_array)
         J = Jacobi_numpy(V_arr[0], V_arr[1], T)
-        print("\n")
-        print(J)
-        print("\n")
-        f_ans = f(V_arr[0], V_arr[1], T)
-        print(f"V:{V_arr}")
-        print(f"f:{f_ans}")
+        #print("\n")
+        #print(J)
+        #print("\n")
+        f_ans = f(V_arr, T)
+        #print(f"V:{V_arr}")
+        #print(f"f:{f_ans}")
         #print(J.shape, f_ans.T.shape)
         
         #print(f_ans.reshape(2,1))
@@ -305,16 +354,26 @@ def newton_two_var(f, Jacobi_uneval, V_arr, T, tol = 10e-1):
         
         i += 1
         if(i>max_it):
+            print("Maximum iterations reached")
             break
-            raise Exception("Maximum iterations reached")
     return V_arr
             
 V_array = np.array([12.6e+3, 35.7])
+#fSolve = scipy.optimize.fsolve(f_tot, x0 = V_array)
 
+print(newton_two_var(f_tot, V_array, 274))
 
-print(newton_two_var(f_tot, Jacobi_uneval, V_array, 274 ))
-        
-        
-print(V_array)
+T_arr = np.linspace(274,647,500)
+V_of_T = np.zeros((len(T_arr),2))
 
-print(f_tot(V_array[0],V_array[1], 274))
+for i in range(len(T_arr)):
+    newton_two_var(f_tot, V_array, T_arr[i])
+    V_of_T[i]= V_array
+
+#BRUKER LOGARITMISK PLOTT FOR Å VISE AT DE MØTES, USIKKER PÅ HVA VI SKAL SI OM ENHETER
+plt.semilogy(T_arr,V_of_T, label = ["$V_g$","$V_v$"]) 
+plt.title("Logaritmisk plot av $V_g$ og $V_v$")
+plt.xlabel("T [K]")
+plt.vlines(T_c,20,10**4, color="gray", label = "$T_c$")
+plt.legend()
+plt.show()
