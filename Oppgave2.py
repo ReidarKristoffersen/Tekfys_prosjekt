d341573982ac25ecc5ef0237d16b4ba029ae41e1
Thientn1 <thiennguyen1033@gmail.com>
2023-03-27 14:59:18 +0200

    plis

--- Oppgave2.py
+++ Oppgave2.py
@@ -5,7 +5,7 @@ Created on Fri Mar 24 16:15:59 2023
 """
 import numpy as np
 import matplotlib.pyplot as plt 
-from scipy import optimize
+from scipy import optimize, interpolate
 
 # Oppgave 2a
 
@@ -60,7 +60,7 @@ def modell_Vv(T, a, b, c, d, e):
 plt.title("Latent varme, L")
 plt.plot(T1, L, label="Exp. L")
 plt.xlabel("Varme [bar*mL / mol")
-plt.ylabel("Volum [mL]")
+plt.ylabel("Volum [mL]") #[J/mol]?
 a_L = optimize.curve_fit(modell_L, T1, L)[0][0]
 b_L = optimize.curve_fit(modell_L, T1, L)[0][1]
 plt.plot(T1, modell_L(T1, a_L, b_L), label="Tilnærming") 
@@ -137,8 +137,55 @@ def simpson(T_start, T_slutt, n, f):
 #     S = (f(a) + f(b) + np.sum(f_x1) + np.sum(f_x2)) * (h/3)
 #     return S
 
-T_0 = np.arange(273.5, 646.5, 1)
-T_1 = np.arange(274.5, 647.5, 1)
+T_0 = np.arange(274, 646, 1)
+T_1 = np.arange(275, 647, 1)
 
-p = simpson(T_0, T_1, 100, clapeyrons)
-plt.plot(((T_0+T_1)/2), p)
+# Leser datafilen
+Vg_data = np.loadtxt("Vg_verdier.txt")
+T_exp = Vg_data[:, 0]                    # Temperaturer [K]
+p_g_exp = (Vg_data[:, 2])                  # Trykk [bar]
+
+p = simpson(T_0, T_1, 100, clapeyrons) #er denne i bar?
+plt.semilogy(((T_0+T_1)/2), p, label = "Simpson")
+plt.semilogy(T_exp,p_g_exp, label = "Eksperimentell")
+plt.xlabel("Temperatur [K]")
+plt.ylabel("Trykk [bar]")
+plt.legend()
+plt.show()
+
+def CubicSpline_interpol():
+    """
+    
+
+    Returns
+    -------
+    None.
+
+    """
+    L_cub = interpolate.CubicSpline(T1, L)
+    Vg_cub = interpolate.CubicSpline(T2, Vg)
+    Vv_cub = interpolate.CubicSpline(T3, Vv)
+    return L_cub, Vg_cub, Vv_cub
+
+L_cubic, Vg_cubic, Vv_cubic = CubicSpline_interpol()
+
+plt.plot(T1, L_cubic(T1), label = "L cubic", color = "green")
+plt.plot(T1, modell_L(T1, a_L, b_L), label = "L curve fit", linestyle="--")
+plt.plot(T1, L, label="Exp. L", linestyle = ":", color = "red")
+plt.legend()
+plt.show()
+
+plt.plot(T2, Vg_cubic(T2), label = "Vg cubic", color = "green")
+plt.plot(T2, modell_Vg(T2, a_Vg, b_Vg), label = "Vg curve fit", linestyle="--")
+plt.plot(T2, Vg, label="Exp. Vg", linestyle = ":", color = "red")
+plt.legend()
+plt.show()
+
+plt.plot(T3, Vv_cubic(T3), label = "Vv cubic", color = "green")
+plt.plot(T3, modell_Vv(T3, a_Vv, b_Vv, c_Vv, d_Vv, e_Vv), label = "Vv curve fit", linestyle="--")
+plt.plot(T3, Vv, label="Exp. Vv", linestyle = ":", color = "red")  
+plt.legend()
+plt.show() 
+
+"hei"
+    
